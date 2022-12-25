8d0d96c3ad123b5f074c38f9ea732cf76e6c99bf
anjue39 <98279987+anjue39@users.noreply.github.com>
2022-12-25 17:27:01 +0800

    Add files via upload

--- utils/clashcheck/start.sh
+++ utils/clashcheck/start.sh
@@ -1,9 +1,10 @@
-#!/usr/bin/env sh
-#Python 3.x
-#pwd /path/to/openit/utils/clashcheck
+#!/bin/sh
+#python 3.x
+#pwd /home/$USER/clashcheck
 #with log ↓          #no log ↓ 
 #nohup ./start.sh &  #nohup ./start.sh > /dev/null 2>&1 &
-#TODO start apache if in docker & start clashcheck with while if no false
+#TODO start clashcheck with while
+#macOS -> `zsh: command not found: timeout` -> https://stackoverflow.com/questions/3504945/timeout-command-on-mac-os-x
 
-if [ "$in_docker" = "true" ]; then apachectl start; else echo "Hello"; fi
-while [ $? -eq 0 ]; do python main.py; done
+nginx
+while true; do timeout -k 20 4m python main.py; done
