cbd1e1ae6d316504c1afd4270a5a9de094056db7
anjue39 <98279987+anjue39@users.noreply.github.com>
2022-12-25 12:19:13 +0800

    Update start.sh

--- utils/clashcheck/start.sh
+++ utils/clashcheck/start.sh
@@ -1,10 +1,9 @@
-#!/bin/sh
-#python 3.x
-#pwd /home/$USER/clashcheck
+#!/usr/bin/env sh
+#Python 3.x
+#pwd /path/to/openit/utils/clashcheck
 #with log ↓          #no log ↓ 
 #nohup ./start.sh &  #nohup ./start.sh > /dev/null 2>&1 &
-#TODO start clashcheck with while
-#macOS -> `zsh: command not found: timeout` -> https://stackoverflow.com/questions/3504945/timeout-command-on-mac-os-x
+#TODO start apache if in docker & start clashcheck with while if no false
 
-nginx
-while true; do timeout -k 20 4m python main.py; done
+if [ "$in_docker" = "true" ]; then apachectl start; else echo "Hello"; fi
+while [ $? -eq 0 ]; do python main.py; done
