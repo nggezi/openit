33d4af3bacfb526e399bd4889a67750663e3bf18
anjue39 <98279987+anjue39@users.noreply.github.com>
2023-03-29 16:24:07 +0800

    Update main.py

--- utils/pool/main.py
+++ utils/pool/main.py
@@ -9,28 +9,6 @@ from yaml.loader import SafeLoader
 
 headers = {'Accept': '*/*', 'Accept-Encoding': 'gzip', 'Connection': 'Keep-Alive', 'User-Agent': 'Clash'}
 
-def local(proxy_list, file):
-    try:
-        with open(file, 'r') as reader:
-            working = yaml.safe_load(reader)
-        data_out = []
-        for x in working['proxies']:
-            data_out.append(x)
-        proxy_list.append(data_out)
-    except:
-        print(file + ": No such file")
-
-def url(proxy_list, link):
-    try:
-        working = yaml.safe_load(requests.get(url=link, timeout=240, headers=headers).text)
-        data_out = []
-        for x in working['proxies']:
-            data_out.append(x)
-        proxy_list.append(data_out)
-    except:
-        print("Error in Collecting " + link )
-        #pass
-
 def fetch(proxy_list, filename):
     current_date = time.strftime("%Y_%m_%d", time.localtime())
     baseurl = 'https://raw.githubusercontent.com/changfengoss/pub/main/data/'
@@ -40,6 +18,17 @@ def fetch(proxy_list, filename):
         data_out.append(x)
     proxy_list.append(data_out)
 
+def url(proxy_list, link):
+    try:
+        data_out=[]
+        working = yaml.safe_load(requests.get(url=link, timeout=240, headers=headers).text)
+        for x in working['proxies']:
+            data_out.append(x)
+        proxy_list.append(data_out)
+    except:
+        #pass
+        print("Error in Collecting " + link )
+
 proxy_list=[]
 if __name__ == '__main__':
     with Manager() as manager:
@@ -47,11 +36,9 @@ if __name__ == '__main__':
         current_date = time.strftime("%Y_%m_%d", time.localtime())
         #print("Today is: " + current_date)
         start = time.time() #time start
-        config = 'config.yaml'
-        with open(config, 'r') as reader:
+        with open('config.yaml', 'r') as reader:
             config = yaml.load(reader, Loader=SafeLoader)
             subscribe_links = config['sub']
-            subscribe_files = config['local']
         directories, total = get_file_list()
         data = parse(directories)
         try:
@@ -68,12 +55,6 @@ if __name__ == '__main__':
         processes=[]
 
         try: #Process开启多线程
-            for i in subscribe_files:
-                p = Process(target=local, args=(proxy_list, i))
-                p.start()
-                processes.append(p)
-            for p in processes:
-                p.join()
             for i in subscribe_links:
                 p = Process(target=url, args=(proxy_list, i))
                 p.start()
