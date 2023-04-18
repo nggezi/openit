69c521a7cb570d3e0631fc751a3461670e163551
anjue39 <98279987+anjue39@users.noreply.github.com>
2023-04-19 04:34:22 +0800

    Update check.py
    add 10mb文件测速

--- utils/clashcheck/check.py
+++ utils/clashcheck/check.py
@@ -3,23 +3,17 @@ import json
 
 
 
-def check(alive, proxy, apiurl, sema, timeout, testurl, testurl-google, testurl-10mb):
+def check(alive, proxy, apiurl,sema,timeout, testurl):
     try:
-        r = requests.get(url=apiurl + '/proxies/' + str(proxy['name']) + '/delay?url='+testurl-google+'&timeout=' + str(timeout), timeout=10)
+        r = requests.get(url=apiurl + '/proxies/' + str(proxy['name']) + '/delay?url='+testurl+'&timeout=' + str(timeout), timeout=10)
         response = json.loads(r.text)
         try:
             if response['delay'] > 0:
-                r = requests.get(url=apiurl + '/proxies/' + str(proxy['name']) + '/delay?url='+testurl+'&timeout=' + str(timeout), timeout=10)
+                r = requests.get(url=apiurl + '/proxies/' + str(proxy['name']) + '/delay?url=https://cachefly.cachefly.net/10mb.test&timeout=' + str(timeout), timeout=10)
                 response = json.loads(r.text)
                 try:
                     if response['delay'] > 0:
-                        r = requests.get(url=apiurl + '/proxies/' + str(proxy['name']) + '/delay?url='+testurl-10mb+'&timeout=' + str(timeout), timeout=15)
-                        response = json.loads(r.text)
-                        try:
-                            if response['delay'] > 0:
-                                alive.append(proxy)
-                        except:
-                            pass
+                        alive.append(proxy)
                 except:
                     pass
         except:
