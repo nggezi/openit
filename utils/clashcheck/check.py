ac0e14689e58b90813160ea4e61f5fe0628633cb
anjue39 <98279987+anjue39@users.noreply.github.com>
2023-04-19 03:40:57 +0800

    Update check.py

--- utils/clashcheck/check.py
+++ utils/clashcheck/check.py
@@ -2,12 +2,29 @@ import requests
 import json
 
 
-def check(alive, proxy, apiurl, sema, timeout, testurl):
+
+def check(alive, proxy, apiurl,sema,timeout, testurl,testurl-google,testurl-10mb):
     try:
-        r = requests.get(url=apiurl + '/proxies/' + str(proxy['name']) + '/delay?url='+testurl+'&timeout=' + str(timeout), timeout=10)
+        r = requests.get(url=apiurl + '/proxies/' + str(proxy['name']) + '/delay?url='+testurl-google+'&timeout=' + str(timeout), timeout=10)
         response = json.loads(r.text)
-        if response['delay'] > 0:
-            alive.append(proxy)
+        try:
+            if response['delay'] > 0:
+                r = requests.get(url=apiurl + '/proxies/' + str(proxy['name']) + '/delay?url='+testurl+'&timeout=' + str(timeout), timeout=10)
+                response = json.loads(r.text)
+                try:
+                    if response['delay'] > 0:
+                        r = requests.get(url=apiurl + '/proxies/' + str(proxy['name']) + '/delay?url='+testurl-10mb+'&timeout=' + str(timeout), timeout=15)
+                        response = json.loads(r.text)
+                        try:
+                            if response['delay'] > 0:
+                                alive.append(proxy)
+                        except:
+                            pass
+                except:
+                    pass
+        except:
+            pass
     except:
         pass
+
     sema.release()
