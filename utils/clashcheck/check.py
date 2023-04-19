8ac8d5cdf52799f27ae5d1af20be940183b587f4
anjue39 <98279987+anjue39@users.noreply.github.com>
2023-04-19 13:16:05 +0800

    Update check.py

--- utils/clashcheck/check.py
+++ utils/clashcheck/check.py
@@ -3,38 +3,46 @@ import json
 
 
 def check(alive, proxies, apiurl, sema, timeout, testurl):
+    # 如果代理池为空，直接返回
     if not proxies:
         return
     
-    first_pass = []
+    first_pass = []  # 用于存放第一轮测试通过的代理
     
-    # Test delay for the first URL and filter proxies with delay > 0
+    # 第一轮测试：测试代理的延迟
     for proxy in proxies:
         try:
+            # 发起测试请求
             r = requests.get(url=apiurl + '/proxies/' + str(proxy['name']) + '/delay?url=' + testurl + '&timeout=' + str(timeout), timeout=10)
             response = json.loads(r.text)
             if 'delay' in response and response['delay'] > 0:
+                # 如果延迟大于0，代表请求成功，将该代理加入第一轮测试通过的代理列表
                 first_pass.append(proxy)
         except:
             pass
     
+    # 如果第一轮测试通过的代理列表为空，直接返回
     if not first_pass:
         return
     
-    second_pass = []
+    second_pass = []  # 用于存放第二轮测试通过的代理
     
-    # Test delay for the second URL and filter proxies with delay > 0
+    # 第二轮测试：测试代理的延迟和下载速度
     for proxy in first_pass:
         try:
+            # 测试代理的延迟
             r = requests.get(url=apiurl + '/proxies/' + str(proxy['name']) + '/delay?url=https://cachefly.cachefly.net/10mb.test&timeout=' + str(timeout), timeout=10)
             response = json.loads(r.text)
             if 'delay' in response and response['delay'] > 0:
+                # 如果延迟大于0，代表请求成功，继续测试下载速度
                 r = requests.get(url=apiurl + '/proxies/' + str(proxy['name']) + '/speed?url=https://cachefly.cachefly.net/10mb.test&timeout=' + str(timeout), timeout=10)
                 response = json.loads(r.text)
                 if 'speed' in response and response['speed'] > 3:
+                    # 如果下载速度大于3MB/s，将该代理加入第二轮测试通过的代理列表
                     second_pass.append(proxy)
         except:
             pass
     
+    # 将第二轮测试通过的代理列表添加到传递进来的代理存活列表中
     alive.extend(second_pass)
     sema.release()
