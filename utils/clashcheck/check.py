023fc7cd88ffd3640be192a76ab34513db0e775e
anjue39 <98279987+anjue39@users.noreply.github.com>
2023-04-19 11:19:47 +0800

    Update check.py

--- utils/clashcheck/check.py
+++ utils/clashcheck/check.py
@@ -1,21 +1,11 @@
 import requests
 import json
 
-
-
-def check(alive, proxy, apiurl,sema,timeout, testurl):
+def check(alive, proxy, apiurl, sema, timeout, testurl):
     try:
         r = requests.get(url=apiurl + '/proxies/' + str(proxy['name']) + '/delay?url='+testurl+'&timeout=' + str(timeout), timeout=10)
         response = json.loads(r.text)
-        try:
-            if 'delay' in response and response['delay'] > 0:
-                r = requests.get(url=apiurl + '/proxies/' + str(proxy['name']) + '/delay?url=https://cachefly.cachefly.net/10mb.test&timeout=' + str(timeout), timeout=10)
-                response = json.loads(r.text)
-                if 'delay' in response and response['delay'] > 0:
-                    alive.append(proxy)
-        except:
-            pass
-    except:
-        pass
-
+        if 'delay' in response and response['delay'] > 0:
+            alive.append(proxy)
+    except: pass
     sema.release()
