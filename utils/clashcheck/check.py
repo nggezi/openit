ef0975522d7cc8f4050e0ee8bcb28c723f3ce2f8
anjue39 <98279987+anjue39@users.noreply.github.com>
2023-04-19 12:52:42 +0800

    Update check.py

--- utils/clashcheck/check.py
+++ utils/clashcheck/check.py
@@ -1,32 +1,40 @@
 import requests
 import json
-from threading import Thread, Semaphore
 
-def check(alive, proxy, apiurl, sema, timeout, testurl):
-    try:
-        r = requests.get(url=apiurl + '/proxies/' + str(proxy['name']) + '/delay?url=' + testurl + '&timeout=' + str(timeout), timeout=10)
-        response = json.loads(r.text)
-        if 'delay' in response and response['delay'] > 0:
+
+def check(alive, proxies, apiurl, sema, timeout, testurl):
+    if not proxies:
+        return
+    
+    first_pass = []
+    
+    # Test delay for the first URL and filter proxies with delay > 0
+    for proxy in proxies:
+        try:
+            r = requests.get(url=apiurl + '/proxies/' + str(proxy['name']) + '/delay?url=' + testurl + '&timeout=' + str(timeout), timeout=10)
+            response = json.loads(r.text)
+            if 'delay' in response and response['delay'] > 0:
+                first_pass.append(proxy)
+        except:
+            pass
+    
+    if not first_pass:
+        return
+    
+    second_pass = []
+    
+    # Test delay for the second URL and filter proxies with delay > 0
+    for proxy in first_pass:
+        try:
             r = requests.get(url=apiurl + '/proxies/' + str(proxy['name']) + '/delay?url=https://cachefly.cachefly.net/10mb.test&timeout=' + str(timeout), timeout=10)
             response = json.loads(r.text)
             if 'delay' in response and response['delay'] > 0:
-                download_time = requests.get(url=testurl, proxies={'http': proxy['proxy'],'https': proxy['proxy']}, timeout=10).elapsed.total_seconds()
-                download_speed = 10 / download_time
-                if download_speed > 3:
-                    alive.append(proxy)
-    except:
-        pass
+                r = requests.get(url=apiurl + '/proxies/' + str(proxy['name']) + '/speed?url=https://cachefly.cachefly.net/10mb.test&timeout=' + str(timeout), timeout=10)
+                response = json.loads(r.text)
+                if 'speed' in response and response['speed'] > 3:
+                    second_pass.append(proxy)
+        except:
+            pass
+    
+    alive.extend(second_pass)
     sema.release()
-
-
-def check_proxies(apiurl, testurl, timeout=10, threads=100):
-    proxies = json.loads(requests.get(apiurl + '/proxies', timeout=10).text)
-    alive = []
-    sema = Semaphore(threads)
-    for proxy in proxies:
-        sema.acquire()
-        t = Thread(target=check, args=(alive, proxy, apiurl, sema, timeout, testurl))
-        t.start()
-    for i in range(threads):
-        sema.acquire()
-    return alive
