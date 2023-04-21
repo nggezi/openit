e6e3a4aed42870b937ee6c63969f4d5c51591c42
anjue39 <98279987+anjue39@users.noreply.github.com>
2023-04-21 22:06:16 +0800

    Update clash.py

--- utils/clashcheck/clash.py
+++ utils/clashcheck/clash.py
@@ -4,7 +4,6 @@ import flag
 import socket
 import maxminddb
 import platform
-import psutil
 import requests
 from tqdm import tqdm
 from pathlib import Path
@@ -80,21 +79,6 @@ def checkenv():
         exit(1)
 
     return clashname, operating_system
-
-
-def checkuse(clashname, operating_system):
-    pids = psutil.process_iter()
-    for pid in pids:
-        if(pid.name() == clashname):
-            if operating_system.startswith('Darwin'):
-                os.kill(pid.pid,9)
-            elif operating_system.startswith('Linux'):
-                os.kill(pid.pid,9)
-            elif operating_system.startswith('Windows'):
-                os.popen('taskkill.exe /pid:'+str(pid.pid))
-            else:
-                print(clashname, str(pid.pid) + " ← kill to continue")
-                exit(1)
 
 
 def filter(config):
