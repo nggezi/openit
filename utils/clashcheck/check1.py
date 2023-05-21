b78b7f791e9f79e8e9effb2db6d540e9d12e8d4d
anjue39 <98279987+anjue39@users.noreply.github.com>
2023-05-21 21:14:28 +0800

    Update check1.py

--- utils/clashcheck/check1.py
+++ utils/clashcheck/check1.py
@@ -1,7 +1,7 @@
 import requests
 import json
 
-def check1(alive, proxy, apiurl, sema, timeout, testurl):
+def check1(alive, proxy, apiurl, sema, timeout, testurl1):
     try:
         r = requests.get(url=apiurl + '/proxies/' + str(proxy['name']) + '/delay?url='+testurl1+'&timeout=' + str(timeout), timeout=10)
         response = json.loads(r.text)
