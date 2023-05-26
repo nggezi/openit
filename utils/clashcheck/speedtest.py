5e6ebb7e9b78a4b915a1ec9dafc8b45bc9299702
anjue39 <98279987+anjue39@users.noreply.github.com>
2023-05-26 08:55:43 +0800

    Update speedtest.py

--- utils/clashcheck/speedtest.py
+++ utils/clashcheck/speedtest.py
@@ -13,9 +13,14 @@ def download_speed_test(download_results, proxy, download_test_url, download_tes
         float: 下载速度（单位：MB/s）
     """
     try:
+        start_time = time.time()
         response = requests.get(download_results, proxy, download_test_url, download_test_timeout, sema_download)
-        speed_in_bytes = len(response.content)
-        speed_in_mb = speed_in_bytes / (1024 * 1024)
+        end_time = time.time()
+        total_time = end_time - start_time
+        latency = response.elapsed.total_seconds()
+        file_size = len(response.content)
+        file_in_mb = file_size / (1024 * 1024)
+        speed_in_mb = file_in_mb / (total_time - latency)
         return speed_in_mb
     except Exception as e:
         print(f"Download speed test failed for proxy {proxy['name']}: {str(e)}")
