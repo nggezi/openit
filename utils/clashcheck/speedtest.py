a7e883009a2f3998c0d8daa6ed1a2de152e5f9e0
anjue39 <98279987+anjue39@users.noreply.github.com>
2023-05-26 14:03:39 +0800

    Update speedtest.py

--- utils/clashcheck/speedtest.py
+++ utils/clashcheck/speedtest.py
@@ -14,14 +14,17 @@ def download_speed_test(download_results, proxy, download_test_url, download_tes
     """
     try:
         start_time = time.time()
-        response = requests.get(download_test_url, proxies=proxy, timeout=download_test_timeout)
+        r = requests.get(download_test_url, proxies=proxy, timeout=download_test_timeout)
+        response = json.loads(r.text)
         end_time = time.time()
         total_time = end_time - start_time
-        latency = response.elapsed.total_seconds()
+        latency = r.elapsed.total_seconds()
         file_size = len(response.content)
         file_in_mb = file_size / (1024 * 1024)
         speed_in_mb = file_in_mb / (total_time - latency)
         proxy['speed'] = speed_in_mb
+                    if 'proxy['speed']' in response and response['proxy['speed']'] >= download_speed_threshold
+                alive.append(proxy)
     except requests.exceptions.RequestException:
         proxy['speed'] = 0  # 请求异常，速度为 0
         latency = 0  # 设置 latency 的默认值
