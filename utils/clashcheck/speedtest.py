7d4ebc9275d7d40507c5599a2b43797b7c409bad
anjue39 <98279987+anjue39@users.noreply.github.com>
2023-05-26 11:04:42 +0800

    Update speedtest.py

--- utils/clashcheck/speedtest.py
+++ utils/clashcheck/speedtest.py
@@ -1,20 +1,16 @@
 import time
 import requests
 
-def download_speed_test(download_results, proxy, download_test_url, download_test_timeout, sema_download, speed_list):
+def download_speed_test(download_results, proxy, download_test_url, download_test_timeout, sema_download):
     """
     下载速度测试
 
     Args:
-        download_results (str): 下载结果保存路径
+        download_results (list): 下载结果保存列表
         proxy (dict): 代理信息
         download_test_url (str): 下载测试文件的 URL
         download_test_timeout (int): 下载超时时间
         sema_download (multiprocessing.Semaphore): 控制并发下载的信号量
-        speed_list (list): 存储下载速度的列表
-
-    Returns:
-        float: 下载速度（单位：MB/s）
     """
     try:
         start_time = time.time()
@@ -26,7 +22,7 @@ def download_speed_test(download_results, proxy, download_test_url, download_tes
         file_in_mb = file_size / (1024 * 1024)
         speed_in_mb = file_in_mb / (total_time - latency)
         proxy['speed'] = speed_in_mb
-        speed_list.append(proxy['speed'])
     except requests.exceptions.RequestException:
         proxy['speed'] = 0  # 请求异常，速度为 0
-        speed_list.append(proxy['speed'])
+
+    download_results.append(proxy)
