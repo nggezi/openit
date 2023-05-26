8496d25ba9a699c3bca6ef513381d64eafbffd34
anjue39 <98279987+anjue39@users.noreply.github.com>
2023-05-26 14:06:54 +0800

    Update speedtest.py

--- utils/clashcheck/speedtest.py
+++ utils/clashcheck/speedtest.py
@@ -2,7 +2,7 @@ import time
 import requests
 import json
 
-def download_speed_test(download_results, proxy, download_test_url, download_test_timeout):
+def download_speed_test(download_results, proxy, download_test_url, download_test_timeout, download_speed_threshold):
     """
     下载速度测试
 
@@ -11,7 +11,7 @@ def download_speed_test(download_results, proxy, download_test_url, download_tes
         proxy (dict): 代理信息
         download_test_url (str): 下载测试文件的 URL
         download_test_timeout (int): 下载超时时间
-        sema_download (multiprocessing.Semaphore): 控制并发下载的信号量
+        download_speed_threshold (float): 下载速度阈值
     """
     try:
         start_time = time.time()
@@ -24,12 +24,11 @@ def download_speed_test(download_results, proxy, download_test_url, download_tes
         file_in_mb = file_size / (1024 * 1024)
         speed_in_mb = file_in_mb / (total_time - latency)
         proxy['speed'] = speed_in_mb
-                    if 'proxy['speed']' in response and response['proxy['speed']'] >= download_speed_threshold
-                alive.append(proxy)
+        if 'speed' in response and response['speed'] >= download_speed_threshold:
+            download_results.append(proxy)
     except requests.exceptions.RequestException:
         proxy['speed'] = 0  # 请求异常，速度为 0
         latency = 0  # 设置 latency 的默认值
         pass
-
 
     download_results.append(proxy)
