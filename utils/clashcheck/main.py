8c68827a0a08e7b741feaf70ca7def54f56f3d33
anjue39 <98279987+anjue39@users.noreply.github.com>
2023-05-26 14:11:36 +0800

    Update main.py

--- utils/clashcheck/main.py
+++ utils/clashcheck/main.py
@@ -91,7 +91,7 @@ if __name__ == '__main__':
             
             for proxy in tqdm(alive, desc="下载测速测试"):
                 sema_download.acquire()
-                p = multiprocessing.Process(target=download_speed_test, args=(download_results, proxy, download_test_url, download_test_timeout))
+                p = multiprocessing.Process(target=download_speed_test, args=(download_results, proxy, download_test_url, download_test_timeout, download_speed_threshold))
                 p.start()
                 processes.append(p)
 
