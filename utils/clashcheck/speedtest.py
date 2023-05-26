443798ae38145efe7a8fc183a10d69a745281ef1
anjue39 <98279987+anjue39@users.noreply.github.com>
2023-05-26 09:13:10 +0800

    Update speedtest.py

--- utils/clashcheck/speedtest.py
+++ utils/clashcheck/speedtest.py
@@ -6,16 +6,18 @@ def download_speed_test(download_results, proxy, download_test_url, download_tes
     下载速度测试
 
     Args:
+        download_results (str): 下载结果保存路径
         proxy (dict): 代理信息
         download_test_url (str): 下载测试文件的 URL
-        download_timeout (int): 下载超时时间
+        download_test_timeout (int): 下载超时时间
+        sema_download (multiprocessing.Semaphore): 控制并发下载的信号量
 
     Returns:
         float: 下载速度（单位：MB/s）
     """
     try:
         start_time = time.time()
-        response = requests.get(download_results, proxy, download_test_url, download_test_timeout, sema_download)
+        response = requests.get(download_test_url, proxies=proxy, timeout=download_test_timeout)
         end_time = time.time()
         total_time = end_time - start_time
         latency = response.elapsed.total_seconds()
