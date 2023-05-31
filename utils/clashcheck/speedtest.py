c948a6d0159677ad371ec6db258c20203852a399
anjue39 <98279987+anjue39@users.noreply.github.com>
2023-05-31 21:54:49 +0800

    Update speedtest.py

--- utils/clashcheck/speedtest.py
+++ utils/clashcheck/speedtest.py
@@ -1,19 +1,23 @@
-import time
 import requests
+import time
 
-def download_speed_test(download_results, proxy, download_test_url, download_test_timeout, download_speed_threads):
-    start_time = time.time()  # 记录开始时间
-
-    # 发起下载请求
+def download_speed_test(download_results, proxy, download_test_url, download_test_timeout, sema1):
     try:
-        response = requests.get(download_test_url, proxies=proxy, timeout=download_test_timeout)
-    except requests.exceptions.RequestException:
-        download_time = None  # 下载失败，将下载时间设为 None
-    else:
-        end_time = time.time()  # 记录结束时间
-        #delay = response.elapsed.total_seconds()  # 获取延迟时间
+        session = requests.Session()
+        session.proxies = {'http': proxy['proxies']['http'], 'https': proxy['proxies']['https']}
+        start_time = time.time()
+        response = session.get(download_test_url, timeout=download_test_timeout)
+        end_time = time.time()
 
-        download_time = end_time - start_time  # 计算下载时间（减去延迟时间）
+        if response.status_code == 200:
+            download_speed = len(response.content) / (end_time - start_time)
+            proxy['download_speed'] = download_speed
+        else:
+            proxy['download_speed'] = None
 
-    # 将下载时间和代理信息保存到共享列表
-    download_results.append({'proxy': proxy, 'download_time': download_time})
+        download_results.append(proxy)
+    except Exception:
+        proxy['download_speed'] = None
+        download_results.append(proxy)
+    finally:
+        sema1.release()
