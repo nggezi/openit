af45a1b610cd17764d1491b39405da3f58280db1
anjue39 <98279987+anjue39@users.noreply.github.com>
2023-05-31 19:48:32 +0800

    Update main.py

--- utils/clashcheck/main.py
+++ utils/clashcheck/main.py
@@ -1,27 +1,3 @@
-"""
-这段 `main.py` 的代码的主要作用是进行代理测试和筛选，包括启动 Clash 进程、执行第一轮测试、执行第二轮测试（如果适用）、将测试结果写入文件，并进行一些清理操作。
-
-代码的主要流程如下：
-1. 初始化配置和环境设置
-2. 启动 Clash 进程
-3. 第一轮测试：使用 testurl 进行代理测试
-4. 根据第一轮测试结果进行筛选
-5. 如果存在第二轮测试的 testurl1，执行第二轮测试
-6. 将第二轮测试结果作为最终结果
-7. 将测试结果写入文件
-8. 清理进程和临时文件
-
-注意事项：
-- 代码使用了多进程进行并发测试，提高测试效率
-- 使用了进度条库 tqdm，用于显示测试进度
-
-如果有第二轮测试，第二轮测试是在第一轮测试的结果之上进行的。
-在第一轮测试完成后，将活跃代理列表 alive 的内容作为第二轮测试的初始数据。
-第二轮测试将使用 testurl1 进行测试，并将第二轮测试的结果作为最终结果。
-所以最后的结果是第二轮测试的最终结果，即第二轮测试中的活跃代理列表 alive。
-如果没有第二轮测试或者 testurl1 为空，则最终结果就是第一轮测试的结果，即第一轮测试中的活跃代理列表 alive。
-"""
-
 import time
 import subprocess
 from multiprocessing import Process, Manager, Semaphore
@@ -29,18 +5,20 @@ from check import check
 from tqdm import tqdm
 from init import init, clean
 from clash import push, checkenv, checkuse
+from speedtest import download_speed_test
 
 if __name__ == '__main__':
     with Manager() as manager:
         alive = manager.list()
         # 初始化配置
-        http_port, api_port, threads, source, timeout, outfile, proxyconfig, apiurl, testurl, testurl1, config = init()
+        http_port, api_port, threads, source, timeout, outfile, proxyconfig, apiurl, testurl, testurl1, download_test_enable, download_test_url, download_test_timeout, download_speed_threshold, download_speed_threads, config = init()
         clashname, operating_system = checkenv()
         checkuse(clashname[2::], operating_system)
         # 启动 Clash 进程
         clash = subprocess.Popen([clashname, '-f', './temp/working.yaml', '-d', '.'])
         processes = []
         sema = Semaphore(threads)
+        sema1 = Semaphore(download_speed_threads)  # 新增的信号量对象
         time.sleep(5)  # 等待 Clash 进程启动
 
         # 第一轮测试，使用 testurl
@@ -52,9 +30,6 @@ if __name__ == '__main__':
         for p in processes:
             p.join()
 
-        # 将第一轮测试的结果作为最终结果
-        alive = list(alive)
-        
         # 如果存在第二轮测试的 testurl1，进行第二轮测试
         if testurl1 and testurl1.strip():
             processes = []
@@ -76,6 +51,24 @@ if __name__ == '__main__':
             # 没有第二轮测试时，将第一轮测试的结果作为最终结果
             alive = list(alive)
             print("只进行了第一次测试，结果数量:", len(alive))
+
+        # 执行下载测速测试（仅在 download_test_enable 为 True 且 download_test_url 存在时进行）
+        if download_test_enable and download_test_url:
+            download_results = manager.list()  # 创建共享的下载结果列表
+            processes = []
+
+            # 下载测速测试，基于第一轮或第二轮测试的活跃代理
+            for proxy in tqdm(alive, desc="Download Speed Test"):
+                sema1.acquire()  # 使用新的信号量对象 sema1
+                p = Process(target=download_speed_test, args=(download_results, proxy, download_test_url, download_test_timeout, sema1))
+                p.start()
+                processes.append(p)
+            for p in processes:
+                p.join()
+
+            # 将下载速度测试的结果作为最终结果
+            download_alive = [proxy for proxy in download_results if 'download_speed' in proxy and proxy['download_speed'] is not None and proxy['download_speed'] >= download_speed_threshold]
+            alive = list(download_alive)
 
         # 将测试结果写入文件
         push(alive, outfile)
