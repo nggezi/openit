272f92afb531768c6bd9f3755b7a8b198c1fb55c
anjue39 <98279987+anjue39@users.noreply.github.com>
2023-05-31 21:12:07 +0800

    Update speedtest.py

--- utils/clashcheck/speedtest.py
+++ utils/clashcheck/speedtest.py
@@ -11,7 +11,7 @@ def download_speed_test(download_results, proxy, download_test_url, download_tes
         download_time = None  # 下载失败，将下载时间设为 None
     else:
         end_time = time.time()  # 记录结束时间
-        delay = response.elapsed.total_seconds()  # 获取延迟时间
+        #delay = response.elapsed.total_seconds()  # 获取延迟时间
 
         download_time = end_time - start_time - delay  # 计算下载时间（减去延迟时间）
 
