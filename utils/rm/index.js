cceb72b5605fbaa97fc1cae4d49084f7a20bdccc
anjue39 <98279987+anjue39@users.noreply.github.com>
2024-10-23 09:19:36 +0800

    增加geoip数据库作为备选来检测国家

--- utils/rm/index.js
+++ utils/rm/index.js
@@ -1,41 +1,24 @@
 const fs = require('fs');
 const location = require('./location');
 const config = require('./config');
+const geoip = require('geoip-lite'); // 使用 geoip-lite 作为备用国家识别工具
 
-// 默认输入路径
+// 输入文件路径，默认为 './url'
 let urls = fs.readFileSync('./url', 'utf8');
 let flags = JSON.parse(fs.readFileSync('./flags.json', 'utf8'));
 
-// 初始化变量
 let urlList = urls.split('\n');
 let resList = [];
 let stringList = [];
 let finalList = [];
 let finalURLs = [];
-let countryList = ['UN']; // 将'unknown'改为'UN'
-let emojiList = ['🏳']; // 默认的旗帜
+let countryList = ['UN'];
+let emojiList = [''];
 let countryCount = { UN: 0 };
 let urlCountryList = { UN: [] };
 
-// 深度检测函数
-async function deepDetectCountry(address) {
-    let country = await location.get(address);
-    if (!country || country === 'unknown') {
-        // 如果 location.get 无法识别国家，则尝试使用备用逻辑
-        if (address.match(/^\d+\.\d+\.\d+\.\d+$/)) {
-            // 简单的 IP 地址检测逻辑，可以扩展为调用外部 IP 数据库服务
-            // 例如，通过判断 IP 的区域划分来推测国家
-            country = 'UN'; // 未识别的情况下使用 'UN' 作为默认值
-        } else {
-            // 其它情况仍使用默认值
-            country = 'UN';
-        }
-    }
-    return country;
-}
-
 async function run() {
-    // 处理flags
+    // 处理 flags
     for (let i = 0; i < flags.length; i++) {
         countryList.push(flags[i].code);
         emojiList.push(flags[i].emoji);
@@ -43,109 +26,120 @@ async function run() {
         urlCountryList[flags[i].code] = [];
     }
 
-    // 解析URL
+    // 解析 URL
     for (let i = 0; i < urlList.length; i++) {
-        let url = urlList[i];
-        switch (url.split('://')[0]) {
-            case 'vmess':
-                let vmessJSON = JSON.parse(Buffer.from(url.split('://')[1], 'base64').toString('utf-8'));
-                vmessJSON.ps = null;
-                resList.push({ type: 'vmess', data: vmessJSON, address: vmessJSON.add });
-                break;
-            case 'trojan':
-                let trojanData = url.split('://')[1].split('#')[0];
-                let trojanAddress = trojanData.split('@')[1].split('?')[0].split(':')[0];
-                resList.push({ type: 'trojan', data: trojanData, address: trojanAddress });
-                break;
-            case 'ss':
-                let ssData = url.split('://')[1].split('#')[0];
-                let ssAddress = ssData.split('@')[1].split('#')[0].split(':')[0];
-                resList.push({ type: 'ss', data: ssData, address: ssAddress });
-                break;
-            case 'ssr':
-                let ssrData = Buffer.from(url.split('://')[1], 'base64').toString('utf-8');
-                let ssrAddress = ssrData.split(':')[0];
-                resList.push({ type: 'ssr', data: ssrData.replace(/remarks=.*?(?=&)/, "remarks={name}&"), address: ssrAddress });
-                break;
-            case 'https':
-                let httpsData = url.split('://')[1].split('#')[0];
-                let httpsAddress = Buffer.from(httpsData.split('?')[0], "base64").toString('utf8').split('@')[1].split(':')[0];
-                resList.push({ type: 'https', data: httpsData, address: httpsAddress });
-                break;
-            default:
-                console.log('未知协议类型: ' + url.split('://')[0]);
-                break;
+        let url = urlList[i].trim();
+        if (!url) continue; // 跳过空行
+        try {
+            let protocol = url.split('://')[0];
+            switch (protocol) {
+                case 'vmess':
+                    let vmessJSON = JSON.parse(Buffer.from(url.split('://')[1], 'base64').toString('utf-8'));
+                    vmessJSON.ps = null;
+                    resList.push({ type: 'vmess', data: vmessJSON, address: vmessJSON.add });
+                    break;
+                case 'trojan':
+                    let trojanData = url.split('://')[1].split('#')[0];
+                    let trojanAddress = trojanData.split('@')[1].split('?')[0].split(':')[0];
+                    resList.push({ type: 'trojan', data: trojanData, address: trojanAddress });
+                    break;
+                case 'ss':
+                    let ssData = url.split('://')[1].split('#')[0];
+                    let ssAddress = ssData.split('@')[1].split('#')[0].split(':')[0];
+                    resList.push({ type: 'ss', data: ssData, address: ssAddress });
+                    break;
+                case 'ssr':
+                    let ssrData = Buffer.from(url.split('://')[1], 'base64').toString('utf-8');
+                    let ssrAddress = ssrData.split(':')[0];
+                    resList.push({ type: 'ssr', data: ssrData.replace(/remarks=.*?(?=&)/, "remarks={name}&"), address: ssrAddress });
+                    break;
+                case 'https':
+                    let httpsData = url.split('://')[1].split('#')[0];
+                    let httpsAddress = Buffer.from(httpsData.split('?')[0], "base64").toString('utf8').split('@')[1].split(':')[0];
+                    resList.push({ type: 'https', data: httpsData, address: httpsAddress });
+                    break;
+                default:
+                    console.log(`未知协议类型: ${protocol}`);
+                    break;
+            }
+        } catch (e) {
+            console.log(`解析 URL 出错: ${url}, 错误: ${e.message}`);
         }
     }
 
-    // 去重并处理国家信息
+    // 去重处理，将对象转换为字符串
     for (let i = 0; i < resList.length; i++) {
         stringList.push(JSON.stringify(resList[i]));
     }
     let afterList = Array.from(new Set(stringList));
+
+    // 转换回对象
     for (let i = 0; i < afterList.length; i++) {
         finalList.push(JSON.parse(afterList[i]));
     }
 
-    // 深度检测国家
+    // 批量检测国家
     for (let i = 0; i < finalList.length; i++) {
-        finalList[i].country = await deepDetectCountry(finalList[i].address);
+        try {
+            let country = await location.get(finalList[i].address);
+            if (!country || country === 'UN') {
+                // 如果无法获取国家，尝试使用 geoip-lite 检查
+                let geo = geoip.lookup(finalList[i].address);
+                country = geo ? geo.country : 'UN';
+            }
+            finalList[i].country = country || 'UN';
+        } catch (e) {
+            finalList[i].country = 'UN';
+            console.log(`获取国家信息出错: ${finalList[i].address}, 错误: ${e.message}`);
+        }
     }
 
     // 生成链接
     for (let i = 0; i < finalList.length; i++) {
         let item = finalList[i];
-        countryCount[finalList[i].country]++;
-        let name = emojiList[countryList.indexOf(finalList[i].country)] + finalList[i].country + ' ' + countryCount[finalList[i].country] + config.nodeAddName;
-        switch (item.type) {
-            case 'vmess':
-                try {
+        let country = item.country || 'UN';
+        countryCount[country]++;
+        let name = emojiList[countryList.indexOf(country)] + country + ' ' + countryCount[country] + config.nodeAddName;
+
+        if (!urlCountryList[country]) {
+            urlCountryList[country] = [];
+        }
+
+        try {
+            switch (item.type) {
+                case 'vmess':
                     item.data.ps = name;
-                    urlCountryList[finalList[i].country].push('vmess://' + Buffer.from(JSON.stringify(item.data), 'utf8').toString('base64'));
-                } catch (e) {
-                    console.log('生成链接错误: ' + e.message + ', 类型: vmess');
-                }
-                break;
-            case 'trojan':
-                try {
-                    urlCountryList[finalList[i].country].push('trojan://' + item.data + '#' + encodeURIComponent(name));
-                } catch (e) {
-                    console.log('生成链接错误: ' + e.message + ', 类型: trojan');
-                }
-                break;
-            case 'ss':
-                try {
-                    urlCountryList[finalList[i].country].push('ss://' + item.data + '#' + encodeURIComponent(name));
-                } catch (e) {
-                    console.log('生成链接错误: ' + e.message + ', 类型: ss');
-                }
-                break;
-            case 'ssr':
-                try {
-                    urlCountryList[finalList[i].country].push('ssr://' + Buffer.from(item.data.replace('{name}', Buffer.from(name, 'utf8').toString('base64')), 'utf8').toString('base64'));
-                } catch (e) {
-                    console.log('生成链接错误: ' + e.message + ', 类型: ssr');
-                }
-                break;
-            case 'https':
-                try {
-                    urlCountryList[finalList[i].country].push('https://' + item.data + '#' + encodeURIComponent(name));
-                } catch (e) {
-                    console.log('生成链接错误: ' + e.message + ', 类型: https');
-                }
-                break;
-            default:
-                break;
+                    urlCountryList[country].push('vmess://' + Buffer.from(JSON.stringify(item.data), 'utf8').toString('base64'));
+                    break;
+                case 'trojan':
+                    urlCountryList[country].push('trojan://' + item.data + '#' + encodeURIComponent(name));
+                    break;
+                case 'ss':
+                    urlCountryList[country].push('ss://' + item.data + '#' + encodeURIComponent(name));
+                    break;
+                case 'ssr':
+                    urlCountryList[country].push('ssr://' + Buffer.from(item.data.replace('{name}', Buffer.from(name, 'utf8').toString('base64')), 'utf8').toString('base64'));
+                    break;
+                case 'https':
+                    urlCountryList[country].push('https://' + item.data + '#' + encodeURIComponent(name));
+                    break;
+                default:
+                    console.log(`未知类型: ${item.type}`);
+                    break;
+            }
+        } catch (e) {
+            console.log(`生成链接错误: ${e.message}, 类型: ${item.type}`);
         }
     }
 
-    // 输出最终结果
-    for (const i in urlCountryList) {
-        if (urlCountryList[i].length > 0) {
-            finalURLs.push(...urlCountryList[i]);
-        }
+    // 合并 URL 列表
+    for (const country in urlCountryList) {
+        finalURLs = finalURLs.concat(urlCountryList[country]);
     }
-    console.log(`去重改名完成，共${urlList.length}个节点，去重后剩余${finalURLs.length}个节点`);
+
+    console.log(`去重改名完成\n一共 ${urlList.length} 个节点，去重 ${urlList.length - finalURLs.length} 个节点，剩余 ${finalURLs.length} 个节点`);
+    
+    // 输出到 './out'
     fs.writeFileSync('./out', finalURLs.join('\n'));
 }
 
