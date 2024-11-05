764d8ab43536dd3849f936fe9d1dae50bfa4aed6
anjue39 <98279987+anjue39@users.noreply.github.com>
2024-11-05 15:36:10 +0800

    Update index.js

--- utils/rm/index.js
+++ utils/rm/index.js
@@ -1,153 +1,129 @@
-const fs = require('fs');
-const location = require('./location');
-const config = require('./config');
+const fs = require('fs')
+const location = require('./location')
+const config = require('./config')
 
-// 默认输入路径'./url'
-let urls = fs.readFileSync('./url', 'utf8');
-let flags = JSON.parse(fs.readFileSync('./flags.json', 'utf8'));
+//此处输入, 当前默认'./url'------↓
+let urls = fs.readFileSync('./url','utf8');
+let flags = JSON.parse(fs.readFileSync('./flags.json','utf8'))
 
-// 默认旗帜设置
-const DEFAULT_COUNTRY = 'UN';
-const DEFAULT_EMOJI = '🌍';
-
-// 初始化变量
 let urlList = urls.split('\n');
 let resList = [];
 let stringList = [];
 let finalList = [];
 let finalURLs = [];
-let countryList = [DEFAULT_COUNTRY];
-let emojiList = [DEFAULT_EMOJI];
-let countryCount = { [DEFAULT_COUNTRY]: 0 };
-let urlCountryList = { [DEFAULT_COUNTRY]: [] };
+let countryList = ['unknown'];
+let emojiList =[''];
+let countryCount = {unknown:0};
+let urlCountryList = {unknown:[]}
 
-// 初始化国家和旗帜数据
-function initializeCountryData() {
-    for (let flag of flags) {
-        countryList.push(flag.code);
-        emojiList.push(flag.emoji);
-        countryCount[flag.code] = 0;
-        urlCountryList[flag.code] = [];
+async function run(){
+    //处理flags
+    for(let i=0;i<flags.length;i++){
+        countryList.push(flags[i].code);
+        emojiList.push(flags[i].emoji);
+        countryCount[flags[i].code] = 0;
+        urlCountryList[flags[i].code] = [];
     }
-}
 
-// 处理并解析 URL
-function parseURLs() {
-    for (let url of urlList) {
-        try {
-            let protocol = url.split('://')[0];
-            let node = {};
+    //解析URL
+    for(let i=0;i<urlList.length;i++){
+        let url = urlList[i];
+        switch (url.split('://')[0]) {
+            case 'vmess':
+                let vmessJSON = JSON.parse(Buffer.from(url.split('://')[1], 'base64').toString('utf-8'));
+                vmessJSON.ps = null
+                resList.push({type: 'vmess', data: vmessJSON, address: vmessJSON.add})
+                break
+            case 'trojan':
+                let trojanData = url.split('://')[1].split('#')[0];
+                let trojanAddress = trojanData.split('@')[1].split('?')[0].split(':')[0];
+                resList.push({type: 'trojan', data: trojanData, address: trojanAddress})
+                break
+            case 'ss':
+                let ssData = url.split('://')[1].split('#')[0];
+                let ssAddress = ssData.split('@')[1].split('#')[0].split(':')[0];
+                resList.push({type: 'ss', data: ssData, address: ssAddress})
+                break
+            case 'ssr':
+                let ssrData = Buffer.from(url.split('://')[1], 'base64').toString('utf-8');
+                let ssrAddress = ssrData.split(':')[0];
+                resList.push({type: 'ssr', data: ssrData.replace(/remarks=.*?(?=&)/, "remarks={name}&"), address: ssrAddress})
+                break
+            case 'https':
+                let httpsData = url.split('://')[1].split('#')[0];
+                let httpsAddress = Buffer.from(httpsData.split('?')[0],"base64").toString('utf8').split('@')[1].split(':')[0]
+                resList.push({type: 'https', data: httpsData, address: httpsAddress})
+                break
+            default:
+                break
+        }
+    }
 
-            switch (protocol) {
-                case 'vmess':
-                    let vmessJSON = JSON.parse(Buffer.from(url.split('://')[1], 'base64').toString('utf-8'));
-                    vmessJSON.ps = null;
-                    node = { type: 'vmess', data: vmessJSON, address: vmessJSON.add };
-                    break;
-                case 'trojan':
-                    let trojanData = url.split('://')[1].split('#')[0];
-                    let trojanAddress = trojanData.split('@')[1].split('?')[0].split(':')[0];
-                    node = { type: 'trojan', data: trojanData, address: trojanAddress };
-                    break;
-                case 'ss':
-                    let ssData = url.split('://')[1].split('#')[0];
-                    let ssAddress = ssData.split('@')[1].split('#')[0].split(':')[0];
-                    node = { type: 'ss', data: ssData, address: ssAddress };
-                    break;
-                case 'ssr':
-                    let ssrData = Buffer.from(url.split('://')[1], 'base64').toString('utf-8');
-                    let ssrAddress = ssrData.split(':')[0];
-                    node = { type: 'ssr', data: ssrData.replace(/remarks=.*?(?=&)/, "remarks={name}&"), address: ssrAddress };
-                    break;
-                case 'https':
-                    let httpsData = url.split('://')[1].split('#')[0];
-                    let httpsAddress = Buffer.from(httpsData.split('?')[0], "base64").toString('utf8').split('@')[1].split(':')[0];
-                    node = { type: 'https', data: httpsData, address: httpsAddress };
-                    break;
-                default:
-                    continue;
+    //去重时要先将对象转为字符串
+    for(let i=0;i<resList.length;i++){
+        stringList.push(JSON.stringify(resList[i]))
+    }
+
+    //去重
+    let afterList = Array.from(new Set(stringList))
+
+    //转回来
+    for(let i=0;i<afterList.length;i++){
+        finalList.push(JSON.parse(afterList[i]))
+    }
+
+    //批量测试国家
+    for(let i=0;i<finalList.length;i++){
+        finalList[i].country = await location.get(finalList[i].address)
+    }
+
+    //变回链接
+    for(let i=0;i<finalList.length;i++){
+        let item = finalList[i];
+        countryCount[finalList[i].country]++
+        let name = emojiList[countryList.indexOf(finalList[i].country)]+finalList[i].country+' '+countryCount[finalList[i].country]+config.nodeAddName
+        switch (item.type){
+            case 'vmess':
+                try{
+                item.data.ps = (name).toString();
+                urlCountryList[finalList[i].country].push('vmess://'+Buffer.from(JSON.stringify(item.data),'utf8').toString('base64'))
+                }catch(e){console.log('vmess node err')}
+                break
+            case 'trojan':
+                try{
+                urlCountryList[finalList[i].country].push('trojan://'+item.data+'#'+(name).toString())
+                }catch(e){console.log('trojan node err')}
+                break
+            case 'ss':
+                try{
+                urlCountryList[finalList[i].country].push('ss://'+item.data+'#'+(name).toString())
+                }catch(e){console.log('ss node err')}
+                break
+            case 'ssr':
+                try{
+                urlCountryList[finalList[i].country].push('ssr://'+Buffer.from(item.data.replace('{name}', Buffer.from((name).toString(),'utf8').toString('base64')),'utf8').toString('base64'))
+                }catch(e){console.log('ssr node err')}
+                break
+            case 'https':
+                try{
+                urlCountryList[finalList[i].country].push('https://'+item.data+'#'+encodeURIComponent(name.toString()))
+                }catch(e){console.log('https node err')}
+                break
+            default:
+                break
+        }
+    }
+    for(const i in urlCountryList){
+        if(urlCountryList[i].length === 0 ){
+        }else{
+            for (let a=0;a<urlCountryList[i].length;a++){
+                finalURLs.push(urlCountryList[i][a])
             }
-
-            resList.push(node);
-        } catch (e) {
-            console.log(`${protocol} node parsing error:`, e.message);
         }
     }
+    console.log(`去重改名完成\n一共${urlList.length}个节点，去重${urlList.length-finalURLs.length}个节点，剩余${finalURLs.length}个节点`)
+    //此处输出, 当前默认'./out'
+    fs.writeFileSync('./out',finalURLs.join('\n'))
 }
 
-// 去重
-function deduplicate() {
-    stringList = resList.map(node => JSON.stringify(node));
-    let afterList = Array.from(new Set(stringList));
-    finalList = afterList.map(nodeStr => JSON.parse(nodeStr));
-}
-
-// 并行处理国家信息
-async function processCountries() {
-    await Promise.all(finalList.map(async (item) => {
-        try {
-            item.country = await location.get(item.address) || DEFAULT_COUNTRY;
-        } catch (e) {
-            console.log('Country lookup error:', e.message);
-            item.country = DEFAULT_COUNTRY;
-        }
-    }));
-}
-
-// 生成最终链接列表
-function generateFinalURLs() {
-    for (let item of finalList) {
-        let country = item.country;
-        let countryEmoji = emojiList[countryList.indexOf(country)] || DEFAULT_EMOJI;
-        countryCount[country] = (countryCount[country] || 0) + 1;
-
-        let nodeName = `${countryEmoji}${country} ${countryCount[country]}${config.nodeAddName}`;
-        
-        try {
-            switch (item.type) {
-                case 'vmess':
-                    item.data.ps = nodeName;
-                    urlCountryList[country].push('vmess://' + Buffer.from(JSON.stringify(item.data), 'utf8').toString('base64'));
-                    break;
-                case 'trojan':
-                    urlCountryList[country].push(`trojan://${item.data}#${encodeURIComponent(nodeName)}`);
-                    break;
-                case 'ss':
-                    urlCountryList[country].push(`ss://${item.data}#${encodeURIComponent(nodeName)}`);
-                    break;
-                case 'ssr':
-                    urlCountryList[country].push('ssr://' + Buffer.from(item.data.replace('{name}', Buffer.from(nodeName, 'utf8').toString('base64')), 'utf8').toString('base64'));
-                    break;
-                case 'https':
-                    urlCountryList[country].push(`https://${item.data}#${encodeURIComponent(nodeName)}`);
-                    break;
-                default:
-                    break;
-            }
-        } catch (e) {
-            console.log(`${item.type} node processing error:`, e.message);
-        }
-    }
-    
-    for (const countryNodes of Object.values(urlCountryList)) {
-        finalURLs.push(...countryNodes);
-    }
-}
-
-// 输出结果
-function outputResults() {
-    console.log(`去重改名完成\n一共${urlList.length}个节点，去重${urlList.length - finalURLs.length}个节点，剩余${finalURLs.length}个节点`);
-    fs.writeFileSync('./out', finalURLs.join('\n'));
-}
-
-// 主运行函数
-async function run() {
-    initializeCountryData();
-    parseURLs();
-    deduplicate();
-    await processCountries();
-    generateFinalURLs();
-    outputResults();
-}
-
-run();
+run()
