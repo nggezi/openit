b19aa3765d0a8626cb9c756b57ae1349f086401f
anjue39 <98279987+anjue39@users.noreply.github.com>
2024-11-26 10:23:54 +0800

    解决password字符格式问题，修复tls相关h2/gprc错误

--- utils/clashcheck/clash.py
+++ utils/clashcheck/clash.py
@@ -1,79 +1,96 @@
 import os
 import yaml
-import flag
-import socket
-import maxminddb
+import flag  # 用于处理国家/地区的旗帜符号
+import socket  # 用于解析服务器地址
+import maxminddb  # 用于通过 IP 查询国家/地区信息
 import platform
 import psutil
 import requests
-from tqdm import tqdm
-from pathlib import Path
+from tqdm import tqdm  # 进度条显示
+from pathlib import Path  # 文件路径操作
 
-
+# 将列表推送到 Clash 配置文件
 def push(list, outfile):
-    country_count = {}
-    count = 1
+    country_count = {}  # 国家计数器，用于命名节点
+    count = 1  # 节点序号
     clash = {'proxies': [], 'proxy-groups': [
             {'name': 'automatic', 'type': 'url-test', 'proxies': [], 'url': 'https://www.google.com/favicon.ico',
              'interval': 300}, {'name': '🌐 Proxy', 'type': 'select', 'proxies': ['automatic']}],
              'rules': ['MATCH,🌐 Proxy']}
+
+    # 打开 GeoIP 数据库，用于解析节点 IP 对应的国家
     with maxminddb.open_database('Country.mmdb') as countrify:
-        for i in tqdm(range(int(len(list))), desc="Parse"):
+        for i in tqdm(range(len(list)), desc="Parse"):
             x = list[i]
             try:
+                # 校验是否为 VMess 节点
                 float(x['password'])
             except:
                 try:
                     float(x['uuid'])
                 except:
                     try:
+                        # 尝试解析节点信息
                         ip = str(socket.gethostbyname(x["server"]))
                     except:
-                        ip = str(x["server"])
+                        ip = str(x["server"])  # 若解析失败则直接使用原 server
                     try:
+                        # 获取对应的国家 ISO 代码
                         country = str(countrify.get(ip)['country']['iso_code'])
                     except:
-                        country = 'UN'
-                    flagcountry = country
+                        country = 'UN'  # 未知国家则用 UN 代替
+                    flagcountry = country  # 使用国家代码作为 flag 标志
                     try:
+                        # 统计国家对应的节点数量，生成节点名称
                         country_count[country] = country_count[country] + 1
                         x['name'] = str(flag.flag(flagcountry)) + " " + country + " " + str(count)
                     except:
                         country_count[country] = 1
                         x['name'] = str(flag.flag(flagcountry)) + " " + country + " " + str(count)
+                    
+                    # 将节点添加到 Clash 配置中
                     clash['proxies'].append(x)
                     clash['proxy-groups'][0]['proxies'].append(x['name'])
                     clash['proxy-groups'][1]['proxies'].append(x['name'])
-                    count = count + 1
+                    count += 1
 
+    # 写入最终配置文件
     with open(outfile, 'w') as writer:
         yaml.dump(clash, writer, sort_keys=False)
 
 
+# 检查系统环境和 Clash 可执行文件
 def checkenv():
-    operating_system = str(platform.system() + '/' +  platform.machine() + ' with ' + platform.node())
-    print('Try to run Clash on '+ operating_system)
+    """
+    检查系统的操作系统和架构，并返回相应的 Clash 可执行文件名称。
+    """
+    operating_system = f"{platform.system()}/{platform.machine()} with {platform.node()}"
+    print('Try to run Clash on ' + operating_system)
+
     if operating_system.startswith('Darwin'):
+        # macOS 系统
         if 'arm64' in operating_system:
-            clashname='./clash-darwin-arm64'
+            clashname = './clash-darwin-arm64'
         elif 'x86_64' in operating_system:
-            clashname='./clash-darwin-amd64'
+            clashname = './clash-darwin-amd64'
         else:
-            print('System is supported(Darwin) but Architecture is not supported.')
+            print('System is supported (Darwin), but Architecture is not supported.')
             exit(1)
     elif operating_system.startswith('Linux'):
+        # Linux 系统
         if 'x86_64' in operating_system:
-            clashname='./clash-linux-amd64'
+            clashname = './clash-linux-amd64'
         elif 'aarch64' in operating_system:
-            clashname='./clash-linux-arm64'
+            clashname = './clash-linux-arm64'
         else:
-            print('System is supported(Linux) but Architecture is not supported.')
+            print('System is supported (Linux), but Architecture is not supported.')
             exit(1)
     elif operating_system.startswith('Windows'):
+        # Windows 系统
         if 'AMD64' in operating_system:
-            clashname='clash-windows-amd64.exe'
+            clashname = 'clash-windows-amd64.exe'
         else:
-            print('System is supported(Windows) but Architecture is not supported.')
+            print('System is supported (Windows), but Architecture is not supported.')
             exit(1)
     else:
         print('System is not supported.')
@@ -82,23 +99,26 @@ def checkenv():
     return clashname, operating_system
 
 
+# 检查 Clash 是否正在运行
 def checkuse(clashname, operating_system):
+    """
+    如果 Clash 进程已存在，终止其进程。
+    """
     pids = psutil.process_iter()
     for pid in pids:
-        if(pid.name() == clashname):
-            if operating_system.startswith('Darwin'):
-                os.kill(pid.pid,9)
-            elif operating_system.startswith('Linux'):
-                os.kill(pid.pid,9)
+        if pid.name() == clashname:
+            if operating_system.startswith('Darwin') or operating_system.startswith('Linux'):
+                os.kill(pid.pid, 9)
             elif operating_system.startswith('Windows'):
-                os.popen('taskkill.exe /pid:'+str(pid.pid))
+                os.popen(f'taskkill.exe /pid:{pid.pid}')
             else:
-                print(clashname, str(pid.pid) + " ← kill to continue")
+                print(f"{clashname}, {pid.pid} ← kill to continue")
                 exit(1)
 
-
+# 过滤和处理 Clash 配置文件中的代理节点
 def filter(config):
     list = config["proxies"]
+    # 定义支持的加密方式等
     ss_supported_ciphers = ['aes-128-gcm', 'aes-192-gcm', 'aes-256-gcm', 'aes-128-cfb', 'aes-192-cfb', 'aes-256-cfb', 'aes-128-ctr', 'aes-192-ctr', 'aes-256-ctr', 'rc4-md5', 'chacha20', 'chacha20-ietf', 'xchacha20', 'chacha20-ietf-poly1305', 'xchacha20-ietf-poly1305']
     ssr_supported_obfs = ['plain', 'http_simple', 'http_post', 'random_head', 'tls1.2_ticket_fastauth', 'tls1.2_ticket_auth']
     ssr_supported_protocol = ['origin', 'auth_sha1_v4', 'auth_aes128_md5', 'auth_aes128_sha1', 'auth_chain_a', 'auth_chain_b']
@@ -106,6 +126,7 @@ def filter(config):
     iplist = {}
     passlist = []
     count = 1
+    # 初始化 Clash 配置文件结构
     clash = {'proxies': [], 'proxy-groups': [
             {'name': 'automatic', 'type': 'url-test', 'proxies': [], 'url': 'https://www.google.com/favicon.ico',
              'interval': 300}, {'name': '🌐 Proxy', 'type': 'select', 'proxies': ['automatic']}],
@@ -116,12 +137,16 @@ def filter(config):
                 x = list[i]
                 authentication = ''
                 x['port'] = int(x['port'])
-              # 以下两行是原来的代码，感觉是错误的。如果加上，vmess节点就没了，也不知道什么原因
-              # 以下两行的作用是检查该字符串是否只包含数字字符。如果是，则将该字符串转换为整数，并将新的整数值存储回"x"字典中的"password"键
-                # if x['password'].isdigit():
-                   # x['password'] = int(x['password'])
-              # 重新修改代码如下一行代码，密码字段统一为字符串类型
-                # x['password'] = str(x['password'])
+                # 统一 password 字段为字符串类型
+                if 'password' in x:
+                    try:
+                        # 强制将 password 转为字符串类型
+                        x['password'] = str(x['password'])
+                    except Exception as e:
+                        print(f"Error processing password for node {x['name']}: {e}")
+                        x['password'] = ''  # 如果处理失败，设置为空字符串或跳过该节点
+                else:
+                    x['password'] = ''  # 如果字段缺失，设置默认值
                 try:
                     ip = str(socket.gethostbyname(x["server"]))
                 except:
@@ -130,31 +155,29 @@ def filter(config):
                     country = str(countrify.get(ip)['country']['iso_code'])
                 except:
                     country = 'UN'
-                           
-                # 1. 合并 grpc 和 h2 类型处理逻辑 (添加)
+
+                # 节点类型校验逻辑
                 if x['type'] in ['grpc', 'h2']:
-                    try:
-                        x['tls'] = True  # 强制启用 TLS 以避免错误
-                        x['name'] = str(flag.flag(country)) + ' ' + str(country) + ' ' + str(count) + ' ' + x['type'].upper()
-                        authentication = 'password'
-                    except:
-                        continue
-                          
+                    # 确保 TLS 开启
+                    if 'tls' not in x or not x['tls']:
+                        x['tls'] = True
+                    x['name'] = f"{str(flag.flag(country))} {country} {count} {x['type'].upper()}"
+                    authentication = 'password'
+                
                 elif x['type'] == 'ss':
                     try:
                         if x['cipher'] not in ss_supported_ciphers:
-                            ss_omit_cipher_unsupported = ss_omit_cipher_unsupported + 1
                             continue
                         if ip in iplist:
-                            ss_omit_ip_dupe = ss_omit_ip_dupe + 1
                             continue
                         else:
                             iplist[ip] = []
                             iplist[ip].append(x['port'])
-                        x['name'] = str(flag.flag(country)) + ' ' + str(country) + ' ' + str(count) + ' ' + 'SSS'
+                        x['name'] = f"{str(flag.flag(country))} {country} {count} SSS"
                         authentication = 'password'
                     except:
                         continue
+                
                 elif x['type'] == 'ssr':
                     try:
                         if x['cipher'] not in ss_supported_ciphers:
@@ -166,80 +189,99 @@ def filter(config):
                         if ip in iplist:
                             continue
                         else:
-                            iplist.append(ip)
+                            iplist[ip] = []
                             iplist[ip].append(x['port'])
                         authentication = 'password'
-                        x['name'] = str(flag.flag(country)) + ' ' + str(country) + ' ' + str(count) + ' ' + 'SSR'
+                        x['name'] = f"{str(flag.flag(country))} {country} {count} SSR"
                     except:
                         continue
+                
                 elif x['type'] == 'vmess':
                     try:
-                        if 'udp' in x:
-                            if x['udp'] not in [False, True]:
-                                continue
-                        if 'tls' in x:
-                            if x['tls'] not in [False, True]:
-                                continue
-                        if 'skip-cert-verify' in x:
-                            if x['skip-cert-verify'] not in [False, True]:
-                                continue
+                        if 'udp' in x and x['udp'] not in [False, True]:
+                            continue
+                        if 'tls' in x and x['tls'] not in [False, True]:
+                            continue
+                        if 'skip-cert-verify' in x and x['skip-cert-verify'] not in [False, True]:
+                            continue
                         if x['cipher'] not in vmess_supported_ciphers:
                             continue
-                        x['name'] = str(flag.flag(country)) + ' ' + str(country) + ' ' + str(count) + ' ' + 'VMS'
+                        x['name'] = f"{str(flag.flag(country))} {country} {count} VMS"
                         authentication = 'uuid'
                     except:
                         continue
+                
                 elif x['type'] == 'trojan':
                     try:
+                        # 校验配置项 'udp' 是否符合布尔值
                         if 'udp' in x:
                             if x['udp'] not in [False, True]:
                                 continue
+                        # 校验配置项 'skip-cert-verify' 是否符合布尔值
                         if 'skip-cert-verify' in x:
                             if x['skip-cert-verify'] not in [False, True]:
                                 continue
+                        # 设置节点名称，格式为 "国家旗帜 ISO码 序号 类型标识"
                         x['name'] = str(flag.flag(country)) + ' ' + str(country) + ' ' + str(count) + ' ' + 'TJN'
-                        authentication = 'password'
+                        authentication = 'password'  # 使用密码作为认证字段
                     except:
                         continue
+                
                 elif x['type'] == 'snell':
                     try:
+                        # 校验配置项 'udp' 是否符合布尔值
                         if 'udp' in x:
                             if x['udp'] not in [False, True]:
                                 continue
+                        # 校验配置项 'skip-cert-verify' 是否符合布尔值
                         if 'skip-cert-verify' in x:
                             if x['skip-cert-verify'] not in [False, True]:
                                 continue
+                        # 设置节点名称，格式为 "国家旗帜 ISO码 序号 类型标识"
                         x['name'] = str(flag.flag(country)) + ' ' + str(country) + ' ' + str(count) + ' ' + 'SNL'
-                        authentication = 'psk'
+                        authentication = 'psk'  # 使用预共享密钥 (PSK) 作为认证字段
                     except:
                         continue
+                
                 elif x['type'] == 'http':
                     try:
+                        # 校验配置项 'tls' 是否符合布尔值
                         if 'tls' in x:
                             if x['tls'] not in [False, True]:
                                 continue
+                        # 设置节点名称，格式为 "国家旗帜 ISO码 序号 类型标识"
                         x['name'] = str(flag.flag(country)) + ' ' + str(country) + ' ' + str(count) + ' ' + 'HTT'
+                        # HTTP 类型暂时不需要认证字段
                         # authentication = 'userpass'
                     except:
                         continue
+                
                 elif x['type'] == 'socks5':
                     try:
+                        # 校验配置项 'tls' 是否符合布尔值
                         if 'tls' in x:
                             if x['tls'] not in [False, True]:
                                 continue
+                        # 校验配置项 'udp' 是否符合布尔值
                         if 'udp' in x:
                             if x['udp'] not in [False, True]:
                                 continue
+                        # 校验配置项 'skip-cert-verify' 是否符合布尔值
                         if 'skip-cert-verify' in x:
                             if x['skip-cert-verify'] not in [False, True]:
                                 continue
+                        # 设置节点名称，格式为 "国家旗帜 ISO码 序号 类型标识"
                         x['name'] = str(flag.flag(country)) + ' ' + str(country) + ' ' + str(count) + ' ' + 'SK5'
+                        # SOCKS5 类型暂时不需要认证字段
                         # authentication = 'userpass'
                     except:
                         continue
+                
                 else:
+                    # 如果节点类型不符合预期，跳过处理
                     continue
 
+                # 避免重复节点
                 if ip in iplist and x['port'] in iplist[ip]:
                     if x[authentication] in passlist:
                         continue
@@ -255,7 +297,7 @@ def filter(config):
                 clash['proxies'].append(x)
                 clash['proxy-groups'][0]['proxies'].append(x['name'])
                 clash['proxy-groups'][1]['proxies'].append(x['name'])
-                count = count + 1
+                count += 1
 
             except:
                 #print('shitwentwrong' + str(x))
